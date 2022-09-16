0ef4d0d25cb24dea0df032d36e52c496ff6be246
gaidaasd00 <swiftgaida@list.ru>
2022-09-16 19:58:02 +0300

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Алексей Гайдуков on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
